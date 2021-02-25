663842cac5abecab0f51ba0a7677a8a5d492332c
ginabethrussell <ginabeth.russell@gmail.com>
2021-02-25 15:15:05 -0500

    updated POM and application properties to use Heroku and Postgres

--- src/main/java/com/lambdaschool/shoppingcart/ShoppingCartApplication.java
+++ src/main/java/com/lambdaschool/shoppingcart/ShoppingCartApplication.java
@@ -2,6 +2,7 @@ package com.lambdaschool.shoppingcart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @EnableJpaAuditing
 @SpringBootApplication
+@PropertySource(value = "file:/Users/ginabethrussell/web38rest.properties", ignoreResourceNotFound = true)
 public class ShoppingCartApplication
 {
     /**
