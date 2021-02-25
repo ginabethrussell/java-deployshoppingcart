cb9900ce41b0f3d1b8a40eb6e33264102a1e626c
ginabethrussell <ginabeth.russell@gmail.com>
2021-02-25 17:55:59 -0500

    add test for add to cart

---
+++ src/test/java/com/lambdaschool/shoppingcart/controllers/CartControllerUnitTestNoDB.java
@@ -0,0 +1,31 @@
+package com.lambdaschool.shoppingcart.controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CartControllerUnitTestNoDB
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void listCartItemsByAuthenticatedUser()
+    {
+    }
+
+    @Test
+    public void addToCart()
+    {
+    }
+}
